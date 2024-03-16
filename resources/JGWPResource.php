8b3e17b5d123a3c27f1826106a26db6fa21f5ae2
Jacob <jtgraham38@gmail.com>
2024-03-15 23:04:49 -0400

    Automated implementation of settings, scripts, and styles.

---
+++ resources/JGWPResource.php
@@ -0,0 +1,34 @@
+<?php
+class JGWPResource extends JGPluginItem{
+
+    protected $filename;
+    protected $type;
+    protected $dependencies;
+
+    public function __construct($plugin, $filename, $dependencies = []){
+        parent::__construct($plugin);   //call parent constructor
+        //setting args
+        $this->filename = $filename;
+        $this->dependencies = $dependencies;
+        $this->id = pathinfo($filename, PATHINFO_FILENAME);
+        $this->extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+    }
+
+    public function add(){
+        //register resource
+        if ($this->extension === 'js'){
+            wp_enqueue_script(
+                $this->plugin->get_prefix() . $this->id . "_" . $this->type,
+                $this->plugin->get_base_url() . "resources/js/" . $this->filename,
+                $this->dependencies,
+            );
+        } else if ($this->extension === 'css'){
+            wp_enqueue_style(
+                $this->plugin->get_prefix() . $this->id . "_" . $this->type,
+                $this->plugin->get_base_url() . "resources/css/" . $this->filename,
+                $this->dependencies,
+            );
+        }
+    }
+}
